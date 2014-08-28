802315c14a6e2df4ebae1f6da19f5d7c1be443c9
Fabio Rocha <fabio@fabiofarias.com.br>
2014-08-28 14:32:04 -0300

    Pecas

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,7 @@
 var meusControllers = angular.module('MeusControllers', []);
 
 /* Controllers */
-meusControllers.controller('MainController', function($rootScope, $scope,CategoriaService,PgtoService) {
+meusControllers.controller('MainController', function($rootScope, $scope) {
 
     $rootScope.$on("$routeChangeStart", function() {
         $rootScope.loading = true;
@@ -15,127 +15,40 @@ meusControllers.controller('MainController', function($rootScope, $scope,Categor
     
 });
 meusControllers.controller('lancamentoController',
-        function($scope, $location, $routeParams) {
-            var oProduto = [{
-                'IdProd':'Teste 1',
-                'Nome':'Nome 1'
-            }];
-            
-            $scope.counter = 0;
+        function($scope, $location, $routeParams,$http) {
             var aList = [];
+            
+            
+            $http.get('http://www.pecas.guru/api/produto?busca=oleo').
+                success(function(data, status, headers, config) {
+                    //$scope.posts = data;
+                    console.log(data);
+                    console.log(status);
+                    console.log(headers);
+                    console.log(config);
+              
+            }).
+            error(function(data, status, headers, config) {
+              // log error
+                
+                    console.log(status);
+                
+            });
+            
+          
+            $scope.counter = 0;
+            $scope.aList = aList;
             
             $scope.change = function() {
                 var counter = $scope.counter++;
-                aList.push(oProduto = {
-                                'IdProd':'Teste ' + counter,
-                                'Nome':'Nome ' + counter
-                            });
+             
             };
             
             $scope.aListLancamento = aList;
 });
-meusControllers.controller('addLancController', function($scope, $location, $routeParams, CategoriaService, PgtoService, LancamentoService) {
+meusControllers.controller('addLancController', function($scope, $location, $routeParams) {
 
     $scope.lancamento = {};
     $scope.lancamentos = [];
 
-    $scope.save = function() {
-        var novoLancamento = {};
-
-        novoLancamento.nome = $scope.lancamento.nome;
-        novoLancamento.pgto = $scope.lancamento.pgto.objectId;
-        novoLancamento.categoria = $scope.lancamento.categoria.objectId;
-        novoLancamento.data = $scope.lancamento.data;
-
-        $scope.lancamentos.push(novoLancamento);
-
-        LancamentoService.save(novoLancamento, function(data) {
-            if (data.objectId != null) {
-                $location.path("/lancamento");
-            }
-        });
-
-        $scope.lancamento = {};
-    };
-
-    CategoriaService.getAll(function(data) {
-        $scope.aListCat = data.results;
-    });
-    PgtoService.getAll(function(data) {
-        $scope.aListPgto = data.results;
-    });
-
-    if ($routeParams.id != null) {
-        $scope.master = {id: $routeParams.id};
-    }
-
-    $scope.update = function(user) {
-        console.log($scope.master);
-        $scope.master = angular.copy(user);
-    };
-
-    $scope.reset = function() {
-        $scope.user = angular.copy($scope.master);
-    };
-
-    $scope.isUnchanged = function(user) {
-        return angular.equals(user, $scope.master);
-    };
-
-    $scope.reset();
-
 });
-
- 
-/*
-// Declare the types.
-var Post = Parse.Object.extend("Post");
-var Comment = Parse.Object.extend("Comment");
- 
-// Create the post
-var myPost = new Post();
-myPost.set("title", "I'm Hungry");
-myPost.set("content", "Where should we go for lunch?");
- 
-// Create the comment
-var myComment = new Comment();
-myComment.set("content", "Let's do Sushirrito.");
- 
-// Add the post as a value in the comment
-myComment.set("parent", myPost);
- 
-// This will save both myPost and myComment
-myComment.save();*/
-/*
-var Comment = Parse.Object.extend("Comment");
-
-var query = new Parse.Query(Comment);
- 
-// Retrieve the most recent ones
-query.descending("createdAt");
- 
-// Only retrieve the last ten
-query.limit(10);
- 
-// Include the post data with each comment
-query.include("Post");
-//query.include(["post.author"]);
-
-
-query.find({
-  success: function(comments) {
-     // console.log(comments.parent);
-     
-    // Comments now contains the last ten comments, and the "post" field
-    // has been populated. For example:
-    for (var i = 0; i < comments.length; i++) {
-        console.log(comments[i].get('content'));
-        var Post = comments[i].get('parent');
-        Post.fetch({
-            success: function(post) {
-              var title = post.get("title");
-               console.log(title);
-            }
-        });
-    }
-  }});*/
--- js/services.js
+++ js/services.js
@@ -1,107 +1,15 @@
 var services = angular.module('MeusServicos', ['ngResource']);
 
-services.service('LancamentoService', function($http) {
-
-    this.save = function(data,callback) {
-        $http({method: 'POST',
-            url: 'https://api.parse.com/1/classes/Lancamento',
-            headers: {
-                'X-Parse-Application-Id': 'ezwTgQirFdjt1dnPiidr0nV1eqr9ARiOa3h43CgL',
-                'X-Parse-REST-API-Key': '8DeetvMlYKqQ3VI6uHp08oOhpBrDsK3eoYXlTsfx',
-                'Content-Type': 'application/json'
-            },
-            data:{
-                  nome:data.nome,
-                  data:data.data,
-                  categoria:{"__op":"AddRelation","objects":[{"__type":"Pointer","className":"Categoria","objectId":data.categoria}]},
-                  pgto:{"__op":"AddRelation","objects":[{"__type":"Pointer","className":"Pgto","objectId":data.pgto}]}
-            }
-            
-        }).success(callback);
-    };
-    this.getAll = function(callback) {
-        $http({method: 'GET',//where={"post":{"__type":"Pointer","className":"Post","objectId":"8TOXdXf3tz"}}
-            url: 'https://api.parse.com/1/classes/Lancamento',
-            headers: {
-                'X-Parse-Application-Id': 'ezwTgQirFdjt1dnPiidr0nV1eqr9ARiOa3h43CgL',
-                'X-Parse-REST-API-Key': '8DeetvMlYKqQ3VI6uHp08oOhpBrDsK3eoYXlTsfx',
-                'Content-Type': 'application/json'
-                
-            }
-        }).success(callback);
-    };
-    
-    
-});
-services.service('CategoriaService', function($http) {
+services.service('BuscaService', function($http) {
 
     this.getAll = function(callback) {
         $http({method: 'GET',
-            url: 'https://api.parse.com/1/classes/Categoria',
+            url: 'http://pecas.guru/api/produto?busca=oleo',
             headers: {
-                'X-Parse-Application-Id': 'ezwTgQirFdjt1dnPiidr0nV1eqr9ARiOa3h43CgL',
-                'X-Parse-REST-API-Key': '8DeetvMlYKqQ3VI6uHp08oOhpBrDsK3eoYXlTsfx'
+                'Content-Type': 'application/json'                
             }
         }).success(callback);
     };
     
-    this.getCategoria = function(id,callback) {        
-        if (id != null) {
-            $http({method: 'GET',
-                url: 'https://api.parse.com/1/classes/Categoria/'+id,
-                headers: {
-                    'X-Parse-Application-Id': 'ezwTgQirFdjt1dnPiidr0nV1eqr9ARiOa3h43CgL',
-                    'X-Parse-REST-API-Key': '8DeetvMlYKqQ3VI6uHp08oOhpBrDsK3eoYXlTsfx'
-                }
-            }).success(callback);
-        };
-    };
-    
-});
-services.service('PgtoService', function($http) {
-
-    this.getAll = function(callback) {
-        $http({method: 'GET',
-            url: 'https://api.parse.com/1/classes/Pgto',
-            headers: {
-                'X-Parse-Application-Id': 'ezwTgQirFdjt1dnPiidr0nV1eqr9ARiOa3h43CgL',
-                'X-Parse-REST-API-Key': '8DeetvMlYKqQ3VI6uHp08oOhpBrDsK3eoYXlTsfx'
-            }
-        }).success(callback);
-    };
-    this.getPgto = function(callback, id) {
-
-        if (id != null) {
-            $http({method: 'GET',
-                url: 'https://api.parse.com/1/classes/Pgto/'+id,
-                headers: {
-                    'X-Parse-Application-Id': 'ezwTgQirFdjt1dnPiidr0nV1eqr9ARiOa3h43CgL',
-                    'X-Parse-REST-API-Key': '8DeetvMlYKqQ3VI6uHp08oOhpBrDsK3eoYXlTsfx'
-                }
-            }).success(callback);
-        };
-    };
-    this.save = function(data,callback) {
-        
-            if(data.objectId == null){
-                var methodType = "POST";
-                var urlTarget = 'https://api.parse.com/1/classes/Pgto';
-            }else{
-                var methodType = "PUT";
-                var urlTarget = 'https://api.parse.com/1/classes/Pgto/'+data.objectId;
-            }
-            
-            $http({method: methodType,
-                url: urlTarget,
-                headers: {
-                    'X-Parse-Application-Id': 'ezwTgQirFdjt1dnPiidr0nV1eqr9ARiOa3h43CgL',
-                    'X-Parse-REST-API-Key': '8DeetvMlYKqQ3VI6uHp08oOhpBrDsK3eoYXlTsfx'
-                },
-                data:{
-                  nome:data.nome
-                }
-            }).success(callback);
-
-    };
     
 });
